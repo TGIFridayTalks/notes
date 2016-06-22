d2d76c441e1635fd31796fedf4d44fb32250f5b3
dra1n <dra1n86@gmail.com>
2016-06-22 21:36:21 +0300

    Use redux.

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
-import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
+import React from 'react'
+import { connect } from 'react-redux'
+
+import {List, ListItem} from 'material-ui/List'
+import Divider from 'material-ui/Divider'
+import {grey400} from 'material-ui/styles/colors'
+import IconButton from 'material-ui/IconButton'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+
+const mapStateToProps = state => ({ notes: state.notes })
 
 const iconButtonElement = (
   <IconButton
@@ -16,78 +19,34 @@ const iconButtonElement = (
   >
     <MoreVertIcon color={grey400} />
   </IconButton>
-);
+)
 
 const rightIconMenu = (
   <IconMenu iconButtonElement={iconButtonElement}>
     <MenuItem>Read</MenuItem>
     <MenuItem>Delete</MenuItem>
   </IconMenu>
-);
+)
 
-const ListExampleMessages = () => (
+const mapNotesToItems = (notes) => notes.map((note, index) => (
+  <div key={index}>
+    <ListItem
+      rightIconButton={rightIconMenu}
+      primaryText={note.title}
+      secondaryText={
+        <p>
+          {note.body}
+        </p>
+      }
+    />
+    <Divider />
+  </div>
+))
+
+export const NoteList = (props) => (
   <List>
-    <Subheader>Today</Subheader>
-    <ListItem
-      rightIconButton={rightIconMenu}
-      primaryText="Brendan Lim"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
-          I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider />
-    <ListItem
-      rightIconButton={rightIconMenu}
-      primaryText="me, Scott, Jennifer"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>Summer BBQ</span><br />
-          Wish I could come, but I&apos;m out of town this weekend.
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider />
-    <ListItem
-      rightIconButton={rightIconMenu}
-      primaryText="Grace Ng"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>Oui oui</span><br />
-          Do you have any Paris recs? Have you ever been?
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider />
-    <ListItem
-      rightIconButton={rightIconMenu}
-      primaryText="Kerem Suer"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>Birthday gift</span><br />
-          Do you have any ideas what we can get Heidi for her birthday? How about a pony?
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-    <Divider />
-    <ListItem
-      rightIconButton={rightIconMenu}
-      primaryText="Raquel Parrado"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>Recipe to try</span><br />
-          We should eat this: grated squash. Corn and tomatillo tacos.
-        </p>
-      }
-      secondaryTextLines={2}
-    />
+    {mapNotesToItems(props.notes)}
   </List>
-);
+)
 
-export default ListExampleMessages;
+export default connect(mapStateToProps)(NoteList)
--- src/main.js
+++ src/main.js
@@ -3,20 +3,30 @@ require('./styles/main.css')
 import "babel-polyfill"
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+import React from 'react'
+import { render } from 'react-dom'
+
+import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
+import App from './components/App'
+
+let store = createStore(reducers)
+
 const Main = () => (
   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </MuiThemeProvider>
 );
 
